c96ead09351745d3580447c969887af4c99499f9
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-10-13 15:53:33 +0300

    Add tasks of practice

--- src/components/MoviesGallary/MoviesGallary.jsx
+++ src/components/MoviesGallary/MoviesGallary.jsx
@@ -1,11 +1,13 @@
-export const MoviesGallary = ({ movies }) => {
+export const MoviesGallary = ({ movies, openModal, deleteMovie }) => {
     return (
         <ul>
-            {movies.map(({id, title}) => {
+            {movies.map(({id, title, votes, image}) => {
                 return (
                     <li key={id}>
                         <h2>{title}</h2>
-                        <button type="button">Delete</button>
+                        <p>Votes: {votes}</p>
+                        <button type="button" onClick={() => {openModal({src: image, alt: title})}}>Open poster</button>
+                        <button type="button" onClick={() => {deleteMovie(id)}}>Delete</button>
                     </li>
                 )
             })}
